1747c41afa238d7fd39b6278c0da7b38904f0e8c
josephna76 <59426134+josephna76@users.noreply.github.com>
2024-02-17 16:13:01 -0800

    Add navbar, darkmode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// animate
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -10,4 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".animate-on-scroll").forEach((item) => {
     observer.observe(item);
   });
+});
+
+// darkmode
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("dark-mode-toggle");
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    // Save the current mode to localStorage
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+    }
+  });
+
+  // Check localStorage to apply dark mode if it was previously enabled
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+  }
 });
